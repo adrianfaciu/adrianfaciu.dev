fddb2658d6e30c5da57cb19fe370aaa475fa1091
Adrian Fâciu <adrian.faciu@gmail.com>
2019-05-10 15:27:01 +0300

    Updating main info

--- config.js
+++ config.js
@@ -1,38 +1,39 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://adrianfaciu.dev",
+  title: "adrianfaciu.dev",
+  subtitle: "My personal blog",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "",
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/"
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about"
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: "Contact me",
+      path: "/pages/contacts"
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "Adrian Fâciu",
+    photo: "/photo.jpg",
+    bio:
+      "Passionate software developer. Learning something new each day. Writing code @visma. Writer for @AngularInDepth.",
     contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
+      email: "adrian.faciu@gmail.com",
+      telegram: "#",
+      twitter: "adrianfaciu",
+      github: "adrianfaciu",
+      rss: "#",
+      vkontakte: "#"
     }
   }
 };
