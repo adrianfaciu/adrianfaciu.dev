af408b7c6ec06e262eb1406131945160290c81ac
Adrian Fâciu <adrian.faciu@gmail.com>
2020-03-29 15:01:36 +0300

    Fixed accessibility for link

--- src/components/Sidebar/Contacts/Contacts.js
+++ src/components/Sidebar/Contacts/Contacts.js
@@ -13,13 +13,14 @@ type Props = {
 const Contacts = ({ contacts }: Props) => (
   <div className={styles['contacts']}>
     <ul className={styles['contacts__list']}>
-      {Object.keys(contacts).map((name) => (
+      {Object.keys(contacts).map(name => (
         <li className={styles['contacts__list-item']} key={name}>
           <a
             className={styles['contacts__list-item-link']}
             href={getContactHref(name, contacts[name])}
             rel="noopener noreferrer"
             target="_blank"
+            aria-label={name}
           >
             <Icon icon={getIcon(name)} />
           </a>
