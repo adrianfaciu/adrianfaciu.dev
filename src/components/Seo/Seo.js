a4aa09b93d8dafc35c9c22ca7946c7026fc0b916
Adrian Fâciu <adrian.faciu@gmail.com>
2020-04-01 20:34:45 +0300

    Open graph and twitter meta tags

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -1,19 +1,45 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import PropTypes from "prop-types";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
 
-const SEO = ({ canonical }) => (
-  <>
-    <Helmet>{canonical && <link rel="canonical" href={canonical} />}</Helmet>
-  </>
-);
+import { useSiteMetadata } from '../../hooks';
+
+const SEO = ({ slug, title, description, image, canonical }: Props) => {
+  const { url } = useSiteMetadata();
+
+  return (
+    <>
+      <Helmet>
+        {slug && <meta property="og:url" content={url + slug} />}
+        {title && <meta property="og:title" content={title} />}
+        {description && (
+          <meta property="og:description" content={description} />
+        )}
+        {image && <meta property="og:image" content={image} />}
+
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:type" content="article" />
+
+        {canonical && <link rel="canonical" href={canonical} />}
+      </Helmet>
+    </>
+  );
+};
 
 export default SEO;
 
 SEO.propTypes = {
-  canonical: PropTypes.string
+  slug: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  canonical: PropTypes.string,
 };
 
 SEO.defaultProps = {
-  canonial: null
+  slug: null,
+  title: null,
+  description: null,
+  image: null,
+  canonical: null,
 };
