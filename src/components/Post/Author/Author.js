e6eec94dcf5d9577e09db29b1a8c437b91a4dc5f
Adrian Fâciu <2067101+adrianfaciu@users.noreply.github.com>
2023-10-14 16:28:18 +0300

    Hide twitter

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -6,32 +6,7 @@ import { useSiteMetadata } from '../../../hooks';
 const Author = ({ title, slug }) => {
   const { author, url } = useSiteMetadata();
 
-  const shareUrl = `https://twitter.com/intent/tweet?text=${title}&url=${url +
-    slug}&via=${author.contacts.twitter}`;
-
-  return (
-    <div className={styles['author']}>
-      <p className={styles['author__bio']}>
-        <a
-          className={styles['author__bio-twitter']}
-          href={shareUrl}
-          target="_blank"
-        >
-          Share on <strong>Twitter</strong>
-        </a>
-      </p>
-      <p className={styles['author__bio']}>
-        <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <strong>{author.name}</strong> on Twitter
-        </a>
-      </p>
-    </div>
-  );
+  return <div className={styles['author']}></div>;
 };
 
 export default Author;
