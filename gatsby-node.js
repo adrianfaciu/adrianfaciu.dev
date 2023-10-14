78a8794a70758818bf4d5eb2e0a80c3d57e15234
Adrian Fâciu <2067101+adrianfaciu@users.noreply.github.com>
2023-10-14 17:52:16 +0300

    Hide invlid css order warning

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,17 @@
 'use strict';
 
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+
+// Remove this plugin to see all the mini css warnings about conflicting order
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new FilterWarningsPlugin({
+        exclude: /mini-css-extract-plugin[^]*Conflicting order. Following module has been added:/,
+      }),
+    ],
+  });
+};
+
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
