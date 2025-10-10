5100aadc44be1fd038631c04e035217352c20d27
Adrian Faciu <adrian.faciu@hey.com>
2025-10-11 00:01:00 +0300

    Add shared articles page

---
+++ src/pages/sharedarticles.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const WhatIReadPage = () => {
+  return (
+    <iframe
+      src="https://reederapp.net/2mR0-bSCTv2wfPi1hG5Hsw"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        border: 'none',
+        margin: 0,
+        padding: 0,
+        display: 'block',
+      }}
+      title="What I Read"
+    />
+  );
+};
+
+export default WhatIReadPage;
