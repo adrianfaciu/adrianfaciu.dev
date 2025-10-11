430da7535e48be27d40c8188f850bcf2450358e4
Adrian Faciu <adrian.faciu@hey.com>
2025-10-12 00:22:02 +0300

    Redirect

--- content/posts/2024-11-30---Technical direction & strategy.md
+++ content/posts/2024-11-30---Technical direction & strategy.md
@@ -3,7 +3,7 @@ title: Technical direction & strategy
 date: 2022-01-25T13:00:37.121Z
 template: post
 draft: true
-slug: /posts/technical-sdirection-trategy/
+slug: /posts/technical-direction-strategy/
 category: Engineering
 tags:
   - Engineering
--- src/components/Social/ShareTo.js
+++ src/components/Social/ShareTo.js
@@ -7,7 +7,7 @@ import * as styles from './ShareTo.module.scss';
 export default function ShareTo({ text, url }) {
   return (
     <div className={styles['share']}>
-      <p>Share this article on</p>
+      <p>Share this blog post on</p>
       <LinkedInShareButton url={url} text={text} />
       <p>or</p>
       <ShareToBlueSkyButton url={url} text={text} />
